3299aed185cf06fd88d81b3036fc63c33cfb9b58
NET\michelle <msj@kommunekredit.dk>
2024-06-27 12:18:46 +0200

    add event handlers and visual feedback to search bar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,19 +1,55 @@
-import React from 'react';
+import React, { act, useState } from 'react';
 import './SearchBar.css'
 
 function SearchBar() {
+    const [term, setTerm] = useState('');
+    const handleTermChange = (event) => {
+        setTerm(event.target.value);
+    }
+
+    const [location, setLocation] = useState('');
+    const handleLocationChange = (event) => {
+        setLocation(event.target.value);
+    }
+
+    const [activeSorting, setActiveSorting] = useState('');
+    const handleSortingClick = (event) => {
+        setActiveSorting(event.target.value);
+    }
+
+    const handleSubmit = () => {
+        console.log(`Searching Yelp with ${term}, ${location}, ${activeSorting}`);
+    }
+
     return (
         <div className='search-bar-header'>
             <span className='filter-group'>
-                <button className='filter-button'>Best Match</button>
-                <button className='filter-button'>Highest Rated</button>
-                <button className='filter-button'>Most Reviewed</button>
+                <button 
+                className={activeSorting === 'best_match' ? 'filter-button-active' : 'filter-button'}
+                value='best_match'
+                onClick={handleSortingClick}>Best Match</button>
+                <button 
+                className={activeSorting === 'highest_rated' ? 'filter-button-active' : 'filter-button'}
+                value='highest_rated'
+                onClick={handleSortingClick}>Highest Rated</button>
+                <button 
+                className={activeSorting === 'most_reviewed' ? 'filter-button-active' : 'filter-button'}
+                value='most_reviewed'
+                onClick={handleSortingClick}>Most Reviewed</button>
             </span>
             <span className='search-group'>
-                <input className='search-box' placeholder="Search businesses"></input>
-                <input className='search-box' placeholder='Where?'></input>
+                <input 
+                className='search-box' 
+                placeholder="Search businesses"
+                onChange={handleTermChange}>
+                </input>
+                <input 
+                className='search-box' 
+                placeholder='Where?'
+                onChange={handleLocationChange}>
+                </input>
             </span>
-            <button className='button'>Let's go!</button>
+            <button className='button' onClick={handleSubmit}>Let's go!</button>
         </div>
     );
 }
